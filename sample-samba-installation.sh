5a7a4efc3130900c362c5cf658d6ff28788f2a77
adamczykjac <adamczyk.jac@gmail.com>
2016-07-07 18:35:30 +0200

    Initial commit.

---
+++ sample-samba-installation.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+# This script installs Samba server with a basic configuration
+
+echo "Installing Samba package"
+apt-get update
+apt-get install -y samba --no-install-recommends
+
+echo "Configuring smb.conf"
+echo "[share]
+  comment = Sample Samba Share
+  path = /srv/samba/share
+  guest ok = yes
+  browsable = yes
+  read only = no
+  create mask = 0755" >> /etc/samba/smb.conf
+
+echo "Changing shared folder permissions"
+mkdir -p /srv/samba/share
+chown nobody.nogroup /srv/samba/share
+chmod 755 /srv/samba/share
+
+echo "Restarting daemons"
+restart smbd
+restart nmbd
